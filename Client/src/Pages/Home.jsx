64aa53971e279ae85732fc22d536d6bf44947e5f
Ujjwal Ojha <148178565+UJJWAL6434@users.noreply.github.com>
2026-04-22 20:09:56 +0530

    Update Home.jsx

--- Client/src/Pages/Home.jsx
+++ Client/src/Pages/Home.jsx
@@ -11,7 +11,7 @@ const Home = () => {
     <div>
       <Hero />
       <div className='py-10'>
-        <h1 className='text-4xl font-bold text-center text-gray-800 mb-4'>Our Courses</h1>
+        <h1 className='text-4xl font-bold text-center text-gray-800 mb-4'>Explore Courses</h1>
         <p className='text-center text-gray-600 mb-12'>Explore our curated courses to boost your skills and career. Whether you're a beginner or an expert, we have something for everyone.</p>
         <div className='max-w-7xl mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6'>
           {
